25d6c9123c2f5a271139505c1d22b1f6a484fb7e
lsl233 <ahands233@gmail.com>
2025-01-07 22:17:21 +0800

    feat: init

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_xWPAF from "logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md";
+import __ASTRO_IMAGE_IMPORT_2ivX5X from "./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md";
+export default new Map([["logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md", __ASTRO_IMAGE_IMPORT_xWPAF], ["./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md", __ASTRO_IMAGE_IMPORT_2ivX5X]]);
+		

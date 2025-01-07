eab89b6b0ea0a005e123f79c51a9ef6f4275f8d3
lsl233 <ahands233@gmail.com>
2025-01-07 22:21:55 +0800

    feat: rm

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,6 @@ import expressiveCode from 'astro-expressive-code'
 import { expressiveCodeOptions } from './src/site.config'
 import icon from 'astro-icon'
 
-import vercel from '@astrojs/vercel/serverless'
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.me',

b9884a1a22f5f2cbce547dc14ad66b4dd70cca76
lsl233 <ahands233@gmail.com>
2025-05-12 10:40:06 +0800

    .

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_xWPAF from "logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md";
 import __ASTRO_IMAGE_IMPORT_2ivX5X from "./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md";
-export default new Map([["logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md", __ASTRO_IMAGE_IMPORT_xWPAF], ["./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md", __ASTRO_IMAGE_IMPORT_2ivX5X]]);
+import __ASTRO_IMAGE_IMPORT_ZWNQBa from "./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F003_%E9%A1%B9%E7%9B%AE%E6%80%BB%E7%BB%93%2Findex.md";
+export default new Map([["logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md", __ASTRO_IMAGE_IMPORT_xWPAF], ["./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F001_markdown-elements%2Findex.md", __ASTRO_IMAGE_IMPORT_2ivX5X], ["./cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2F003_%E9%A1%B9%E7%9B%AE%E6%80%BB%E7%BB%93%2Findex.md", __ASTRO_IMAGE_IMPORT_ZWNQBa]]);
 		

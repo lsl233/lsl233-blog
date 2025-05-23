8065299954c0003d6615da4564c37cf2409b6ca6
lsl233 <ahands233@gmail.com>
2025-05-23 20:11:17 +0800

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
+	site: 'https://lsl233.com',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
